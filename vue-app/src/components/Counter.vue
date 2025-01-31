d2b9ac489e9c6b88fa11470ecfccd6646ee527f9
Cesar Cordoba <cordobacesar29@gmail.com>
2025-01-30 21:05:36 -0300

    Counter vue component

---
+++ vue-app/src/components/Counter.vue
@@ -0,0 +1,19 @@
+  <script setup>
+    import { ref } from 'vue';
+    
+    const count = ref(0);
+    
+    const increment = () => count.value++;
+    const decrement = () => count.value--;
+    const reset = () => count.value = 0;
+  </script>
+
+<template>
+  <h1>Contador: {{ count }}</h1>
+  <div style="text-align: center; padding: 20px; display: flex; justify-content: center; gap: 1rem;">
+    <button @click="decrement">-</button>
+    <button @click="increment">+</button>
+    <button @click="reset">Resetear</button>
+  </div>
+</template>
+
